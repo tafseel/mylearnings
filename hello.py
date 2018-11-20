27313b435d3fa58451b1d8cd9549468c71ecd902
tafseel <tafseelahmadkhan@gmail.com>
2018-11-20 21:43:08 +0530

    first versiob

---
+++ hello.py
@@ -0,0 +1,24 @@
+def hello():
+	i = 10
+	print("Hello world: ", i)
+	
+def print_file(file_name):
+	f = open(file_name, "r")
+	lines = f.readlines()
+	for line in lines:
+		print(line)
+	f.close()
+
+def write_file(file_name):
+	f = open(file_name, "w+")
+	f.write("hello")
+	f.write("\tworld\n")
+	f.write("new line")
+	f.close()
+	
+if __name__ == '__main__':
+	import sys
+	hello()
+	print("===========file content is====================")
+	print_file(sys.argv[1])
+	write_file(sys.argv[2])
